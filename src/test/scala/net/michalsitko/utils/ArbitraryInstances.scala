a171efc73802aaccc4adbb7778532ffa833765fe
Michal Sitko <pierwszy1@gmail.com>
2017-06-06 16:10:10 +0200

    first Arbitrary Instances implementation

--- src/test/scala/net/michalsitko/utils/ArbitraryInstances.scala
+++ src/test/scala/net/michalsitko/utils/ArbitraryInstances.scala
@@ -1,24 +1,22 @@
 package net.michalsitko.utils
 
-import java.util
-
 import org.scalacheck.{Arbitrary, Gen}
 
-import scala.annotation.tailrec
 import scala.xml._
 import scala.collection.JavaConverters._
 
 trait ArbitraryInstances {
   def arbitraryNode(depth: Int): Arbitrary[Node] = ???
 
-  val alphanumGen: Gen[String] = Gen.alphaNumStr.map(_.take(10))
+  // TODO: Not sure if retryUnit is the way to go
+  val alphanumGen: Gen[String] = Gen.alphaStr.map(_.take(10)).retryUntil(_.nonEmpty)
 
   val attrGen = for {
     attrName <- alphanumGen
     attrVal  <- alphanumGen
   } yield new UnprefixedAttribute(attrName, new Text(attrVal), Null)
 
-  private val maxAttrsLength = 5
+  private val maxAttrsLength = 2
 
   val attrsGen = for {
     length      <- Gen.choose(0, maxAttrsLength)
@@ -36,63 +34,28 @@ trait ArbitraryInstances {
     attrs <- linkedAttrs
   } yield newElem(label, attrs, children)
 
-  private val maxDirectChilden = 5
-
-//  def elemOfDepth(depth: Int): Gen[Elem] = {
-//    def loop(inside: Gen[Elem], depth: Int, siblingsN: Int): Gen[List[Elem]] = {
-//      if(depth == 0) {
-//        Gen.choose(1, maxDirectChilden).flatMap(n => Gen.listOfN(n, inside))
-//      } else {
-//        for {
-//          label <- alphanumGen
-//          attrs <- linkedAttrs
-//          inner <- loop(inside, depth - 1)
-//        } yield List(newElem(label, attrs, inner))
-//      }
-//    }
-//    loop(leafElem(List(Text("Hello XML"))), depth).map(_.head)
-//  }
+  private val maxDirectChilden = 3
 
   def elemOfDepth(depth: Int): Gen[Elem] = {
-    @tailrec
-    def loop(depth: Int, parents: Gen[List[Elem]], children: Gen[List[List[Elem]]]): Gen[List[Elem]] = {
+    // TODO: non-tail recursion
+    def loop(depth: Int): Gen[List[Elem]] = {
       if(depth == 0) {
-        for {
-          parentList <- parents
-          childrenList <- children
-        } yield parentList.zip(childrenList).map {
-          case (parent, children) => parent.copy(child = children)
-        }
+        Gen.choose(1, maxDirectChilden).flatMap(n => Gen.listOfN(n, leafElem(List(Text("Hello XML")))))
       } else {
-        println(s"bazinga $depth")
-        val abc: Gen[(List[Elem], util.ArrayList[List[Elem]])] = for {
-          parentList <- parents
-          childrenList <- children
+        for {
           newChildrenNumber <- Gen.choose(1, maxDirectChilden)
-          newParents = parentList.zip(childrenList).map {
-            case (parent, children) =>
-              parent.copy(child = children)
-          }
-          newChildren = List.fill(newParents.size)(Gen.listOfN(newChildrenNumber, leafElem(List(Text("Hello XML")))))
-          nn <- Gen.sequence(newChildren)
-        } yield (newParents, nn)
-
-        val args: Gen[(List[Elem], List[List[Elem]])] = abc.map(t => (t._1, t._2.asScala.toList))
-        loop(depth - 1, args.map(_._1), args.map(_._2))
+          children          <- Gen.sequence(List.fill(newChildrenNumber)(loop(depth - 1)))
+          currentNodes      <- Gen.sequence(children.asScala.toList.map(ch => leafElem(ch)))
+          currentNodesList  <- currentNodes.asScala.toList
+        } yield currentNodesList
       }
     }
 
-    val tmp: Gen[List[Elem]] = for {
-      newChildrenNumber <- Gen.choose(1, maxDirectChilden)
-      ch <- Gen.listOfN(newChildrenNumber, leafElem(List(Text("Hello XML"))))
-    } yield ch
-    loop(depth, leafElem(List.empty).map(List(_)), tmp.map(ch => List(ch))).map(_.head)
+    loop(depth - 1).map(_.head)
   }
 
   def elem(depth: Int) = {
-    def loop(elem: Elem): Elem = {
-      elem.par
-    }
+    elemOfDepth(depth)
   }
 
   def newElem(label: String, attributes: MetaData, children: List[Node]) =
