72cfa4357ce25db1fe95ad7246b52756a9b6c4e2
Michal Sitko <pierwszy1@gmail.com>
2017-06-23 10:53:27 +0200

    add ParseBench

---
+++ bench/src/main/scala/net/michalsitko/xml/bench/ParseBench.scala
@@ -0,0 +1,17 @@
+package net.michalsitko.xml.bench
+
+import java.util.concurrent.TimeUnit
+
+import net.michalsitko.xml.parsing.XmlParser
+import org.openjdk.jmh.annotations._
+
+import scala.xml.XML
+
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@State(Scope.Benchmark)
+class ParseBench {
+  @Benchmark def parseWithLens = XmlParser.parse(Roundtrip.example.input).right.get
+
+  @Benchmark def parseWithStd = XML.loadString(Roundtrip.example.input)
+}
