9b711be4239c36e1fb259e81a43ebb04c359a0f2
Michał Sitko <pierwszy1@gmail.com>
2018-04-11 21:53:44 +0200

    publish settings

--- project/Common.scala
+++ project/Common.scala
@@ -49,8 +49,7 @@ object Common {
     scalaVersion := commonScalaVersion,
     crossScalaVersions := commonCrossScalaVersions,
     coverageHighlighting := true,
-    coverageScalacPluginVersion := "1.3.0",
-    version := "0.1.0-SNAPSHOT"
+    coverageScalacPluginVersion := "1.3.0"
   )
 
 }
